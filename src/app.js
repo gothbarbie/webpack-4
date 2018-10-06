6118b3d3777beaec12259c049908c140fc61be15
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-06 14:53:55 +0200

    refactor App-component

--- src/app.js
+++ src/app.js
@@ -1,12 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
+import { hot } from 'react-hot-loader'
 
-function render(Component) {
-  ReactDOM.render(
-    <AppContainer>
-      <h1>Welcome to React</h1>
-    </AppContainer>,
-    document.getElementById('root')
-  )
-}
+const App = () => <h1>Welcome to React</h1>
+
+export default hot(module)(App)
