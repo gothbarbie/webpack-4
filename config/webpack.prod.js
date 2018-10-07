4ee605e1259a1da297477dcbf6f1a7a94c74d17f
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-07 10:56:19 +0200

    feat: add env support and re move ExtractTextPlugin as it breaks build

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,85 +1,108 @@
 const path = require('path')
 const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+// const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
-module.exports = {
-  entry: {
-    main: './src/client.js',
-  },
-  mode: 'production',
-  output: {
-    filename: '[name]-bundle.js',
-    path: path.resolve(__dirname, '../dist'),
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCSSExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-          },
-        ],
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              attrs: ['img:src'],
+module.exports = env => {
+  return {
+    entry: {
+      main: ['./src/client.js'],
+    },
+    mode: 'production',
+    output: {
+      filename: '[name]-bundle.js',
+      path: path.resolve(__dirname, '../dist'),
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-loader',
             },
-          },
-        ],
-      },
-      {
-        test: /\.(jpg|gif|png|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'images/[name].[ext]',
+          ],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCSSExtractPlugin.loader,
             },
-          },
-        ],
-      },
+            {
+              loader: 'css-loader',
+            },
+          ],
+        },
+        {
+          test: /\.sass$/,
+          use: [
+            {
+              loader: 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'postcss-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+              options: {
+                attrs: ['img:src'],
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(jpg|gif|png|svg)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'images/[name].[ext]',
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      // new ExtractTextPlugin('[name].css'),
+      new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.css$/g,
+        cssProcessor: require('cssnano'),
+        cssProcessorOptions: {
+          discardComments: { removeAll: true, canPrint: true },
+        },
+      }),
+      new MiniCSSExtractPlugin({
+        filename: '[name]-[contenthash].css',
+      }),
+      new HTMLWebpackPlugin({
+        template: './src/index.html',
+        inject: true,
+        title: 'Webpack 4',
+      }),
+      new webpack.NamedModulesPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify(env.NODE_ENV),
+        },
+      }),
     ],
-  },
-  plugins: [
-    new ExtractTextPlugin('[name].css'),
-    new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /\.css$/g,
-      cssProcessor: require('cssnano'),
-      cssProcessorOptions: {
-        discardComments: { removeAll: true, canPrint: true },
-      },
-    }),
-    new webpack.NamedModulesPlugin(),
-    new HTMLWebpackPlugin({
-      template: './src/index.html',
-      inject: true,
-      title: 'Webpack 4',
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-  ],
+  }
 }
